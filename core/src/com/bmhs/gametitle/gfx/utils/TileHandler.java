29f81caf8fb91136f6ba54b837da76aed6203f6d
rupertJones05 <owensito2005@gmail.com>
2024-01-26 12:37:52 -0800

    hi

--- core/src/com/bmhs/gametitle/gfx/utils/TileHandler.java
+++ core/src/com/bmhs/gametitle/gfx/utils/TileHandler.java
@@ -49,8 +49,8 @@ public class TileHandler {
         for(int r = 0; r < sheet.getHeight(); r+=spriteOnSheetHeight) {
             for(int c = 0; c < sheet.getWidth(); c+=spriteOnSheetWidth) {
                 boolean solidFound = false;
-                for(int pixelR = 0; pixelR < Tile.ON_SCREEN_DEFAULT_HEIGHT; pixelR++) {
-                    for(int pixelC = 0; pixelC < Tile.ON_SCREEN_DEFAULT_WIDTH; pixelC++) {
+                for(int pixelR = 0; pixelR < spriteOnSheetHeight; pixelR++) {
+                    for(int pixelC = 0; pixelC < spriteOnSheetWidth; pixelC++) {
                         if(pixmap.getPixel(c+pixelC, r+pixelR) != 0) {
                             solidFound = true;
                         }
